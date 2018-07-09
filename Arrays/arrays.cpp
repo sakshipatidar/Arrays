3a6cef2ad5daf896e8dd4c40850e2617aa62f8e4
justi <justi@192.168.254.3>
2018-07-09 00:44:22 -0400

    update

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -9,10 +9,35 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
+	int values[3];
 
+	values[0] = 1;
+	values[1] = 22;
+	values[2] = 33;
 
-return 0;
+	cout << values[3] << endl;
+
+	//short hand
+	int valuesA[4] = { 1, 2, 4, 8 };
+
+	for (int i = 0; i < 4; i++) {
+
+		cout << valuesA[i] * valuesA[i] << endl;
+	}
+
+	int emptyArr[8] = { };
+
+	for (int i = 0; i < 8; i++) {
+		cout << i+1 << "  " << flush;
+	}
+	cout << endl;
+	for (int i = 0; i < 8; i++) {
+
+		cout << emptyArr[i] * emptyArr[i] << ", " << flush;
+	}
+
+	return 0;
 
 }
